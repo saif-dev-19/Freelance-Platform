a83798e779bee5b867e4a2fc3259e62298e3ff06
Mahfuz <anonymous.safu@gmail.com>
2025-09-24 22:48:09 +0600

    edit user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -4,12 +4,12 @@ from djoser.serializers import UserSerializer
 
 
 
-class CustomUserCreateSerializer(UserCreateSerializer):
+class CustomUserCreateSerializer(UserCreateSerializer): #post er jonno
     class Meta(UserCreateSerializer.Meta):
         fields = ['id','email','password','first_name','last_name','role','address','phone_number']
 
 
-class CustomUserSerializer(UserSerializer):
+class CustomUserSerializer(UserSerializer): #get,put,patch er jonno
     class Meta(UserSerializer.Meta):
         fields = ['id','email','first_name','last_name','role','address','phone_number','is_staff', 'is_superuser']
-        read_only_fields = ['is_staff', 'is_superuser']
+        read_only_fields = ['email','role','is_staff', 'is_superuser']

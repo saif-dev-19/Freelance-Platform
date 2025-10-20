0787e9d477800a3b55ba699055a86ae5d9316852
Mahfuz <anonymous.safu@gmail.com>
2025-10-20 23:52:55 +0600

    swagger problem solved

--- users/serializers.py
+++ users/serializers.py
@@ -7,12 +7,13 @@ from rest_framework import serializers
 class CustomUserCreateSerializer(UserCreateSerializer): #post er jonno
     class Meta(UserCreateSerializer.Meta):
         fields = ['id','email','password','first_name','last_name','role','address','phone_number']
-
+        ref_name = 'CustomUserCreate'
 
 class CustomUserSerializer(UserSerializer): #get,put,patch er jonno
     class Meta(UserSerializer.Meta):
         fields = ['id','email','first_name','last_name','role','address','phone_number','is_staff', 'is_superuser']
         read_only_fields = ['email','role','is_staff', 'is_superuser']
+        ref_name = 'CustomUser'
 
 User = get_user_model()
 
@@ -20,3 +21,4 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+        ref_name = 'MyUserModelSerializer'
